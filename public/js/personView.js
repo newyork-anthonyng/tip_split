17823386ef3dda524886d9c4f7093322af58ecdb
General Assembly | Anthony Ng <newyork.anthonyng@gmail.com>
2016-01-02 17:30:54 -0500

    Add custom directive for Person

---
+++ public/js/personView.js
@@ -0,0 +1,15 @@
+angular.module('MyApp')
+  .directive('person', personView);
+
+function personView() {
+  var directive = {};
+
+  directive.restrict = 'E';
+  directive.replace = true;
+  directive.templateUrl = '_personView.html';
+  directive.scope = {
+    name: '@'
+  };
+
+  return directive;
+}
