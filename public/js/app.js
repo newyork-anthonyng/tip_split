ef056ed5ffa75efd678a9e2acafd661e1789257d
General Assembly | Anthony Ng <newyork.anthonyng@gmail.com>
2016-01-02 14:28:54 -0500

    Add calculateTip function

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,40 @@ var myApp = angular.module('MyApp', []);
 
 /** Controller **/
 myApp.controller('MyController', MyController);
+
 function MyController() {
   var self = this;
-  self.testMessage = 'Hello World';
+  self.checkAmount;
+  self.taxAmount;
+  self.tipAmount;
+  self.numberOfPeople = 1;
+
+  // tip percentages
+  self.tipPercentage_low = .15;
+  self.tipPercentage_med = .17;
+  self.tipPercentage_hi  = .20;
+  self.tipPercentage     = self.tipPercentage_med;
+
+  // per person amount
+  self.perPerson_check;
+  self.perPerson_tax;
+  self.perPerson_tip;
+
+  self.calculateTip = function() {
+    // console.log('calculating tip');
+    // console.log(self.tipPercentage);
+    // console.log(self.checkAmount);
+    self.tipAmount = self.tipPercentage * self.checkAmount;
+  };
+
+  self.updatePerPerson = function() {
+
+  };
+
+  self.updateCalculations = function() {
+    self.calculateTip();
+    self.updatePerPerson();
+  };
+
+
 }
