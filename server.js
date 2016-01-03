b06386aa93ca3305c38365429094b7f776c3b75f
General Assembly | Anthony Ng <newyork.anthonyng@gmail.com>
2016-01-03 17:57:08 -0500

    Fix typo of process.env.PORT

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.get('/', (req, res) => {
   res.json({ SUCCESS: true });
 });
 
-const server = app.listen(process.env.port || 3000, () => {
+const server = app.listen(process.env.PORT || 3000, () => {
   console.log('Express server running.');
 });
